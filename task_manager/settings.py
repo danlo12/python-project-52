c420f85ce661c774905e0fc17fa8e05d5e71786b
danlo <danlo12@mail.ru>
2024-10-01 20:33:14 +0300

    add .env to gitignore

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j+wt8d-=gr#yx*e7hfudj1irq6@-nrt16ib0&(028pngnd#b0z'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
