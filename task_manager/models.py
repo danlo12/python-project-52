85c8c7e9f7e0033c144efbe8de4f524d343798b7
danlo <danlo12@mail.ru>
2024-11-19 20:05:01 +0300

    step 4. add tests

--- task_manager/models.py
+++ task_manager/models.py
@@ -21,6 +21,7 @@ class CustomUser(AbstractUser):
     password = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Status(models.Model):
     name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
