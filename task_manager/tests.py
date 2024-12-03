728c957db9909d1ae7a6caaf263e29738adc93d2
danlo <danlo12@mail.ru>
2024-12-03 17:11:10 +0300

    add tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from task_manager.models import CustomUser , Status
+from task_manager.models import CustomUser , Status, Label, Task
 from django.contrib.auth import get_user_model
 
 class UserRegistrationTest(TestCase):
@@ -146,3 +146,139 @@ class StatusesDeleteViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('statuses'))
         self.assertFalse(Status.objects.filter(name='Status to be deleted').exists())
+
+class LabelsListViewTest(TestCase):
+
+    def setUp(self):
+        # Создаем пользователя
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Active')
+        self.url = reverse('labels')
+
+    def test_labels_list_authenticated(self):
+        # Логинимся и проверяем, что страница статусов доступна
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Active')
+
+class LabelsCreateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.url = reverse('label_create')
+
+    def test_create_label_authenticated(self):
+        # Логинимся и создаем новый статус
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url, {'name': 'New Label'})
+        self.assertEqual(response.status_code, 302)  # Статус 302, т.к. будет редирект на страницу списка
+        self.assertRedirects(response, reverse('labels'))
+        self.assertTrue(Label.objects.filter(name='New Label').exists())
+
+class LabelUpdateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Old Label')
+        self.url = reverse('label_update', args=[self.label.id])
+
+    def test_update_label_authenticated(self):
+        # Логинимся и обновляем статус
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url, {'name': 'Updated Label'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('labels'))
+        self.label.refresh_from_db()
+        self.assertEqual(self.label.name, 'Updated Label')
+
+class LabelDeleteViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Label to be deleted')
+        self.url = reverse('label_delete', args=[self.label.id])
+
+    def test_delete_label_authenticated(self):
+        # Логинимся и удаляем статус
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('labels'))
+        self.assertFalse(Status.objects.filter(name='Label to be deleted').exists())
+
+
+class TasksListViewTest(TestCase):
+
+    def setUp(self):
+        # Создаем пользователя
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Test Label')
+        self.status = Status.objects.create(name='Test Status')
+        self.task = Task.objects.create(name='Active',description="test",status=self.status,performer=self.user,creator=self.user)
+        self.task.labels.set([self.label])
+        self.url = reverse('tasks')
+
+    def test_tasks_list_authenticated(self):
+        # Логинимся и проверяем, что страница статусов доступна
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Active')
+
+
+class TaskCreateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Test Label')
+        self.status = Status.objects.create(name='Test Status')
+        self.task = Task.objects.create(name='Test Task',description="test",status=self.status,performer=self.user,creator=self.user)
+        self.task.labels.set([self.label])
+        self.url = reverse('tasks_create')
+
+    def test_create_task_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url, {'name': 'New Task',"description": "test", "status": self.status.id, "performer": self.user.id,"labels": [self.label.id]})
+        self.assertEqual(response.status_code, 302)  # Статус 302, т.к. будет редирект на страницу списка
+        self.assertRedirects(response, reverse('tasks'))
+        self.assertTrue(Task.objects.filter(name='New Task').exists())
+
+
+class TaskUpdateViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Test Label')
+        self.status = Status.objects.create(name='Test Status')
+        self.task = Task.objects.create(name='Old Task',description="test",status=self.status,performer=self.user,creator=self.user)
+        self.task.labels.set([self.label])
+        self.url = reverse('tasks_update', args=[self.task.id])
+
+    def test_update_task_authenticated(self):
+        # Логинимся и обновляем статус
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url, {'name': 'Updated Task',"description": "test", "status": self.status.id, "performer": self.user.id,"labels": [self.label.id]})
+        self.assertEqual(response.status_code, 302)  # Статус 302, т.к. будет редирект на страницу списка
+        self.assertRedirects(response, reverse('tasks'))
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.name, 'Updated Task')
+
+
+class TaskDeleteViewTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.label = Label.objects.create(name='Test Label')
+        self.status = Status.objects.create(name='Test Status')
+        self.task = Task.objects.create(name='Task to be deleted', description="test",status=self.status,performer=self.user,creator=self.user)
+        self.task.labels.set([self.label])
+        self.url = reverse('tasks_delete', args=[self.task.id])
+
+    def test_delete_task_authenticated(self):
+        # Логинимся и удаляем статус
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks'))
+        self.assertFalse(Task.objects.filter(name='Task to be deleted').exists())
