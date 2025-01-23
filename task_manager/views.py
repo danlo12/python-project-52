610afbaa17b74010e87cc0f1ef777b7ad92de244
danlo <danlo12@mail.ru>
2025-01-23 16:19:23 +0300

    test

--- task_manager/views.py
+++ task_manager/views.py
@@ -22,13 +22,14 @@ class UserLoginView(LoginView):
     template_name = 'login.html'
     form_class = CustomLoginForm
     success_url = reverse_lazy('home')
-
     def form_invalid(self, form):
         for field in form.errors:
             for error in form.errors[field]:
                 messages.error(self.request, f"{error}")
         return super().form_invalid(form)
-
+    def form_valid(self, form):
+        messages.success(self.request, "Вы залогинены")
+        return super().form_valid(form)
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('home')
